68a9ef0351079a9e47ff47584d5db4dc0ae23659
Sellucas <lucassellmachado@gmail.com>
2023-08-15 20:03:48 -0300

    feat: navbar animation

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,14 +1,40 @@
 'use client'
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
+
+import Magnetic from '@/components/ui/magnetic';
 
 import styles from './style.module.scss'
 import Nav from './nav';
 
 const Home = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY >= window.innerHeight) {
+        setIsVisible(true);
+        window.removeEventListener('scroll', handleScroll);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      controls.start({ opacity: 1, y: 0 });
+    }
+  }, [isVisible, controls]);
 
   return (
     <>
@@ -17,13 +43,23 @@ const Home = () => {
           SELLUCAS<span>.</span>
         </Link>
 
-        <div className={styles.header}>
-          <div onClick={() => { setIsActive(!isActive) }} className={styles.button}>
-            <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
-          </div>
-        </div>
+        {isVisible &&
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+            className={styles.header}
+            initial={{ opacity: 0, y: -20 }}
+            animate={controls}
+          >
+            <Magnetic>
+              <div onClick={() => { setIsActive(!isActive) }} className={styles.button}>
+                <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+              </div>
+            </Magnetic>
+          </motion.div>
+        }
 
-      </div>
+      </div >
       <AnimatePresence mode="wait">
         {isActive && <Nav />}
       </AnimatePresence>
